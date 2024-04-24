9fc92ecb961c42054c1b81b8fabc9cce879bff53
Aditya Singh <as1671109@gmail.com>
2024-04-24 16:14:20 +0530

    Added MVVM

---
+++ app/src/main/java/com/ascoder1109/countermvvm/CounterViewModel.kt
@@ -0,0 +1,22 @@
+package com.ascoder1109.countermvvm
+
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+
+class CounterViewModel():ViewModel() {
+    private val _repository: CounterRepository = CounterRepository()
+    private val _count = mutableStateOf(_repository.getCounter().count)
+
+    // Expose the count as an immutable state
+    val count:MutableState<Int> = _count
+
+    fun increment(){
+       _repository.increment()
+        _count.value = _repository.getCounter().count
+    }
+    fun decrement(){
+        _repository.decrement()
+        _count.value = _repository.getCounter().count
+    }
+}
